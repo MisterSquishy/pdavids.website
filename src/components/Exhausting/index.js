760c2c88fd2b5a49557f6a437cf66f58fc4e4bac
Pete Davids <pdavids@drift.com>
2020-10-04 14:14:34 -0400

    exhausting is aptly named

--- src/components/Exhausting/index.js
+++ src/components/Exhausting/index.js
@@ -1,41 +1,64 @@
 import React from 'react'
 import Typist from 'react-typist';
 import Confetti from 'react-confetti'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
 
 import './style.css'
 
 export default ({
   onDone
 }) => {
-  return <>
-    <Confetti initialVelocityY={50} confettiSource={{ x: window.innerWidth/2, y: window.innerHeight }}/>
-    <h1>
-      TA-DA!
-    </h1>
-    <Typist cursor={{show: false}} onTypingDone={onDone}>
-      <Typist.Delay ms={3000} />
-      <p>
-        Oh well that's clearly too much
-      </p>
-      <Typist.Delay ms={500} />
-      <p>
-        What are we even doing here
-      </p>
-      <Typist.Delay ms={500} />
-      <p>
-        I mean clearly this site is unusable
-      </p>
-      <Typist.Delay ms={500} />
-      <p>
-        Sorry bout all this
-      </p>
-      <Typist.Delay ms={500} />
-      <p>
-        Alright fine let's just go for something more...{' '}
-        <Typist.Delay ms={500} />
-        straightforward
-      </p>
-      <Typist.Delay ms={1000} />
-    </Typist>
-  </>
+  return <div className='exhausting-page'>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+      integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+      crossorigin="anonymous"
+    />
+    <Confetti initialVelocityY={50} initialVelocityX={-10} confettiSource={{ x: window.innerWidth, y: window.innerHeight }}/>
+    <Confetti initialVelocityY={50} initialVelocityX={10} confettiSource={{ y: window.innerHeight }}/>
+    <Container>
+      <Row>
+        <Col>
+          <h1 className='header'>TA-DA!!!</h1>
+          <Typist cursor={{show: false}} onTypingDone={onDone}>
+            <Typist.Delay ms={3000} />
+              <p>
+                Oh well that's clearly too much
+              </p>
+              <Typist.Delay ms={500} />
+              <p>
+                What are we even doing here
+              </p>
+              <Typist.Delay ms={500} />
+              <p>
+                I mean clearly this site is unusable
+              </p>
+              <Typist.Delay ms={500} />
+              <p>
+                Sorry bout all this
+              </p>
+              <Typist.Delay ms={500} />
+              <p>
+                Alright fine let's just go for something more...{' '}
+              </p>
+              <Typist.Delay ms={500} />
+              <p>
+                reasonable
+              </p>
+              <Typist.Delay ms={1000} />
+          </Typist>
+        </Col>
+        <Col>
+          <Image
+            src={`${window.location.origin}/penguin-icon.png`}
+            width={250}
+            className="exhausting-image" />
+        </Col>
+      </Row>
+    </Container>
+  </div>
 }
