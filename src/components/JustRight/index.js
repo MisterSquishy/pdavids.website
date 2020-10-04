0015649f2bffa14acf2125f0556fd433076a608f
Pete Davids <pdavids@drift.com>
2020-10-04 14:27:07 -0400

    just right

--- src/components/JustRight/index.js
+++ src/components/JustRight/index.js
@@ -7,5 +7,15 @@ export default ({
 }) => {
   return <>
     <h1>Peter Davids</h1>
+    <h2>Builder, tinkerer, problem solver</h2>
+    <p>Some stuff I've built lately:</p>
+    <ul>
+    <li>
+        <a href='http://collatz.pdavids.website'>Collatz</a> -- toy visualization of the Collatz conjecture
+      </li>
+      <li>
+        <a href='http://game.pdavids.website'>Scout</a> -- top-down RPG
+      </li>
+    </ul>
   </>
 }
