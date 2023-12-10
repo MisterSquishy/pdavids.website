8e073946dbe260831339ef59ad7834cd1a20c729
MisterSquishy <pdavids@comcast.net>
2023-12-10 15:41:27 -0500

    content

--- src/components/JustRight/index.js
+++ src/components/JustRight/index.js
@@ -2,19 +2,20 @@ import React from 'react'
 
 import './style.css'
 
-export default ({
-  onDone
-}) => {
+export default () => {
   return <>
     <h1>Peter Davids</h1>
-    <h2>Builder, tinkerer, problem solver</h2>
-    <p>Some stuff I've built lately:</p>
+    <h2>Builder, problem solver</h2>
+    <p>This site isn't super well maintained, check my <a href='https://github.com/MisterSquishy'>github profile</a> to see what I've been up to lately</p>
     <ul>
-    <li>
-        <a href='http://collatz.pdavids.website'>Collatz</a> -- toy visualization of the Collatz conjecture
+      <li>
+        <a href='https://alldaisy.com/'>Daisy</a> -- a whole company! I built the entire <a href='https://app.alldaisy.com'>SaaS app</a> end to end
       </li>
       <li>
-        <a href='http://game.pdavids.website'>Scout</a> -- top-down RPG
+        <a href='https://apps.apple.com/us/app/freyr-sunset/id1605500146'>Freyr</a> -- an iPhone app that predicts sunset quality
+      </li>
+      <li>
+        <a href='http://game.pdavids.website'>Scout</a> -- top-down RPG sandbox where you can play fetch with my childhood dog
       </li>
     </ul>
   </>
